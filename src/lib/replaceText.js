697ac4b0d352fccc03d9251abbe89d5878b412a6
ato lash <hal-shu-sato@users.noreply.github.com>
2021-06-21 23:45:51 +0900

    refactor: modularize scripts
    #9

---
+++ src/lib/replaceText.js
@@ -0,0 +1,12 @@
+/**
+ * Replaces innerText.
+ *
+ * @param {string} selector - A string of id.
+ * @param {string} text - A text to replace.
+ */
+function replaceText(selector, text) {
+  const element = document.getElementById(selector);
+  if (element) element.innerText = text;
+}
+
+module.exports = replaceText;
