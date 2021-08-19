3fbd8f4ba164f988e0e0492319f78c1e4dd71b6e
ato lash <hal-shu-sato@users.noreply.github.com>
2021-08-19 20:40:10 +0900

    build(Electron Forge): adjust the files to be ignored and set the information for Linux

--- forge.config.js
+++ forge.config.js
@@ -5,20 +5,19 @@ module.exports = {
       unpackDir: 'node_modules/7zip-bin',
     },
     ignore: [
-      '(?!node_modules)/\\.github',
-      '(?!node_modules)/\\.husky',
-      '(?!node_modules)/\\.vscode',
-      '(?!node_modules)/\\.czrc$',
-      '(?!node_modules)/\\.editorconfig$',
-      '(?!node_modules)/\\.eslint',
-      '(?!node_modules)/\\.gitignore$',
-      '(?!node_modules)/\\.prettier',
-      '(?!node_modules)/CHANGELOG\\.md$',
-      '(?!node_modules)/CODE_OF_CONDUCT\\.md$',
-      '(?!node_modules)/README\\.md$',
-      '(?!node_modules)/README\\.ja\\.md$',
-      '(?!node_modules)/commitlint\\.config\\.js$',
-      '(?!node_modules)/forge\\.config\\.js$',
+      /^(?!.*node_modules).*\/\.github$/,
+      /^(?!.*node_modules).*\/\.husky$/,
+      /^(?!.*node_modules).*\/\.vscode$/,
+      /^(?!.*node_modules).*\/\.czrc$/,
+      /^(?!.*node_modules).*\/\.editorconfig$/,
+      /^(?!.*node_modules).*\/\.eslint/,
+      /^(?!.*node_modules).*\/\.gitignore$/,
+      /^(?!.*node_modules).*\/\.prettier/,
+      /^(?!.*node_modules).*\/CHANGELOG\.md$/,
+      /^(?!.*node_modules).*\/CODE_OF_CONDUCT\.md$/,
+      /^(?!.*node_modules).*\/CONTRIBUTING(?!.*\/).*\.md$/,
+      /^(?!.*node_modules).*\/README(?!.*\/).*\.md$/,
+      /^(?!.*node_modules).*\/(?!.*\/).*config\.js$/,
     ],
   },
   makers: [
@@ -35,11 +34,21 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          maintainer: 'ato lash',
+          homepage: 'http://halshusato.starfree.jp/ato_lash/apm/',
+        },
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        options: {
+          maintainer: 'ato lash',
+          homepage: 'http://halshusato.starfree.jp/ato_lash/apm/',
+        },
+      },
     },
   ],
   publishers: [
