2a8b80559143669a6330617236ca159427b84bb7
mitosagi <54105954+mitosagi@users.noreply.github.com>
2022-03-05 10:41:02 +0900

    build: add eslint-webpack-plugin

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,3 +1,4 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = [new ForkTsCheckerWebpackPlugin()];
+module.exports = [new ForkTsCheckerWebpackPlugin(), new ESLintPlugin()];
