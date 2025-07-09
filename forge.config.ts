2b44726b39d82dd988082fdfeaf971b895eb773d
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-07-09 18:40:01 +0900

    build: add ThirdPartyNotices.txt handling and related scripts

--- forge.config.ts
+++ forge.config.ts
@@ -15,6 +15,7 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    extraResource: 'ThirdPartyNotices.txt',
   },
   rebuildConfig: {},
   makers: [
