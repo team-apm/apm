51cc921f3891d560d083e8ba6370d3895295a476
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-07-09 18:47:17 +0900

    build: update forge configuration with path imports and detailed maker options

--- forge.config.ts
+++ forge.config.ts
@@ -9,20 +9,40 @@ import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
 import { PublisherGithub } from '@electron-forge/publisher-github';
 
+import path from 'node:path';
+
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
   packagerConfig: {
+    executableName: 'apm',
+    icon: './icon/apm',
     asar: true,
     extraResource: 'ThirdPartyNotices.txt',
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
-    new MakerRpm({}),
-    new MakerDeb({}),
+    new MakerSquirrel({
+      name: 'apm',
+      exe: 'apm.exe',
+      authors: 'Team apm',
+      iconUrl: path.join(__dirname, 'icon/apm.ico'),
+    }),
+    new MakerZIP({}, ['win32', 'darwin', 'linux']),
+    new MakerRpm({
+      options: {
+        homepage: 'https://team-apm.github.io/apm/',
+        icon: path.join(__dirname, 'icon/apm1024.png'),
+      },
+    }),
+    new MakerDeb({
+      options: {
+        maintainer: 'Team apm',
+        homepage: 'https://team-apm.github.io/apm/',
+        icon: path.join(__dirname, 'icon/apm1024.png'),
+      },
+    }),
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
