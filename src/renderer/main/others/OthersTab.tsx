f083f7a7efd53707329c9f2d36a99b03849a97d4
hal-shu-sato <28973861+hal-shu-sato@users.noreply.github.com>
2026-08-23 06:12:48 +0900

    refactor(others): その他タブを react-bootstrap のコンポーネントに置き換える
    外部リンクの a.btn は Button の href 指定に置き換えた。href があるときは
    a タグに role="button" と tabindex を付けて描画するため、旧マークアップの
    手書き属性と同じになる。遷移は止めないので will-navigate による外部
    ブラウザ起動もそのまま。
    
    btn-group / btn-group-vertical は ButtonGroup にした。Bootstrap が
    規定する role="group" が付く点だけ旧マークアップと異なる(縦並びの方は
    手書きでは付け忘れていた)。

--- src/renderer/main/others/OthersTab.tsx
+++ src/renderer/main/others/OthersTab.tsx
@@ -1,4 +1,12 @@
 import React, { type JSX } from 'react';
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  Col,
+  Container,
+  Row,
+} from 'react-bootstrap';
 import { TRPCReact } from '../../trpc';
 
 /**
@@ -12,98 +20,95 @@ function OthersTab() {
   const quitApp = TRPCReact.quitApp.useMutation();
 
   return (
-    <div className="container-lg py-2">
-      <div className="row my-2">
-        <div className="card">
-          <div className="card-body">
-            <h3 className="card-title">その他</h3>
-            <div className="row mb-3">
-              <p className="col-sm-6 col-form-label">
+    <Container fluid="lg" className="py-2">
+      <Row className="my-2">
+        <Card>
+          <Card.Body>
+            <Card.Title as="h3">その他</Card.Title>
+            <Row className="mb-3">
+              <Col as="p" sm={6} className="col-form-label">
                 <span className="app-name">{appName}</span>について
-              </p>
-              <div className="col-sm-6 btn-group-vertical">
-                <button
-                  type="button"
-                  className="btn btn-primary"
+              </Col>
+              <ButtonGroup vertical className="col-sm-6">
+                <Button
+                  variant="primary"
                   id="open-about-window"
                   onClick={() => openAboutWindow.mutate()}
                 >
                   このアプリについて
-                </button>
+                </Button>
 
-                <div className="btn-group" role="group">
-                  <a
+                <ButtonGroup>
+                  <Button
+                    variant="primary"
                     href="https://github.com/team-apm/apm"
-                    role="button"
-                    className="btn btn-primary"
                   >
                     <i className="bi bi-github"></i> GitHub
-                  </a>
-                  <a
+                  </Button>
+                  <Button
+                    variant="primary"
                     href="https://discord.gg/YEQRqnGsG2"
-                    role="button"
-                    className="btn btn-primary"
                   >
                     <i className="bi bi-discord"></i> Discord
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <p className="col-sm-6 col-form-label">
+                  </Button>
+                </ButtonGroup>
+              </ButtonGroup>
+            </Row>
+            <Row className="mb-3">
+              <Col as="p" sm={6} className="col-form-label">
                 プラグイン&スクリプトデータの作成
-              </p>
-              <div className="col-sm-6">
-                <a
+              </Col>
+              <Col sm={6}>
+                <Button
+                  variant="primary"
+                  className="w-100"
                   href="https://team-apm.github.io/apm-web/"
-                  role="button"
-                  className="btn btn-primary w-100"
                 >
                   開く <i className="bi bi-box-arrow-up-right"></i>
-                </a>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <p className="col-sm-6 col-form-label">
+                </Button>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col as="p" sm={6} className="col-form-label">
                 機能要求・バグ報告（外部ブラウザが開きます）
-              </p>
-              <div className="col-sm-3">
-                <a
+              </Col>
+              <Col sm={3}>
+                <Button
+                  variant="primary"
+                  className="w-100"
                   href="https://docs.google.com/forms/d/e/1FAIpQLSf0N-X_u_abi8rrWHVDdiK3YeYuQ7J1f8bQAy6QTD-OR94DWQ/viewform?usp=sf_link"
-                  role="button"
-                  className="btn btn-primary w-100"
                 >
                   Googleフォーム <i className="bi bi-box-arrow-up-right"></i>
-                </a>
-              </div>
-              <div className="col-sm-3">
-                <a
+                </Button>
+              </Col>
+              <Col sm={3}>
+                <Button
+                  variant="primary"
+                  className="w-100"
                   href="https://github.com/team-apm/apm/issues"
-                  role="button"
-                  className="btn btn-primary w-100"
                 >
                   <i className="bi bi-github"></i> GitHub (要アカウント)
                   <i className="bi bi-box-arrow-up-right"></i>
-                </a>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <p className="col-sm-6 col-form-label"></p>
-              <div className="col-sm-6">
-                <button
-                  type="button"
-                  className="btn btn-primary w-100"
+                </Button>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col as="p" sm={6} className="col-form-label"></Col>
+              <Col sm={6}>
+                <Button
+                  variant="primary"
+                  className="w-100"
                   id="quit-app"
                   onClick={() => quitApp.mutate()}
                 >
                   終了
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                </Button>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
   );
 }
 
